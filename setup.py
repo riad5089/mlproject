aa8ec875ac804ce5747d5c9a425a0f6308bf6f32
Md Riad <riad5089@gmail.com>
2024-02-02 21:50:52 +0600

    updated

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPEN_E_DOT='-e.'
+def get_requirements(file_path: str) -> List[str]:
+    requirements=[]
+    with open(file_path, 'r') as file_obj:
+        requirements=file_obj.readlines()
+
+        requirements=[req.replace('\n', "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='ML_PROJECT',
+    version='0.0.1',
+    author="MD Abdur Sobhan Riad",
+    author_email='riad5089@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    )
