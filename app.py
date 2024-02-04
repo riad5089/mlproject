e7e04631e2f5eabf4908ae324f7dde1ececfc686
Md Riad <riad5089@gmail.com>
2024-02-05 00:37:43 +0600

    updated

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if __name__=="__main__":
 
     try:
         #data_ingestion_config=DataIngestionConfig
-        data_ingestion=DataIngestion
+        data_ingestion=DataIngestion()
         data_ingestion.initiate_data_ingestion()
         
     except Exception as e:
