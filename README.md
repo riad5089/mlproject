bb095a28b65c8206b5aadb434c77c54ce3a1a8f0
Md Riad <riad5089@gmail.com>
2024-02-10 10:16:46 +0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## End to End Data Science Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/riad5089/mlproject.mlflow \
+MLFLOW_TRACKING_USERNAME=riad5089 \
+MLFLOW_TRACKING_PASSWORD=b98a37ee98e9d59a346135f14cb8ee568bc00c8b \
+python script.py
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,64 +1,27 @@
-# import os
-# import sys
-# from src.mlproject.exception import CustomException
-# from src.mlproject.logger import logging
-# import pandas as pd
-# from dotenv import load_dotenv
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import r2_score
-# import pymysql
-
-# # import pickle
-# import numpy as np
-
-# load_dotenv()
-
-# host=os.getenv("host")
-# user=os.getenv("user")
-# password=os.getenv("password")
-# db=os.getenv('db')
-
-
-
-# def read_sql_data():
-#     logging.info("Reading SQL database started")
-#     try:
-#         mydb=pymysql.connector.connect(
-#             host=host,
-#             user=user,
-#             password=password,
-#             db=db
-#         )
-#         logging.info("Connection Established",mydb)
-#         df=pd.read_sql_query('Select * from students',mydb)
-#         print(df.head())
-
-#         return df
-
-
-
-#     except Exception as ex:
-#         raise CustomException(ex)
-
 import os
 import sys
-from src.mlproject.exception import CustomException 
+from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
+from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+import pymysql
+
 import pickle
 import numpy as np
 
-from dotenv import load_dotenv
-import pymysql
 load_dotenv()
 
-host=os.getenv('host')
-user=os.getenv('user')
-password=os.getenv('password')
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
 db=os.getenv('db')
 
+
+
 def read_sql_data():
-    logging.info("Reading from mysql database")
+    logging.info("Reading SQL database started")
     try:
         mydb=pymysql.connect(
             host=host,
@@ -66,9 +29,10 @@ def read_sql_data():
             password=password,
             db=db
         )
-        logging.info("Connection established",mydb)
-        df=pd.read_sql_query("select * from students",mydb)
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
+
         return df
 
 
@@ -88,3 +52,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
